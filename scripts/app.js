aef1dab6fcb94c7346167c805e0ce8f73cd9aa46
w3villa-akshya <akshya.chaudhary@w3villa.com>
2022-09-29 15:54:25 +0530

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElement = document.querySelectorAll('.hid den');
+ hiddenElement.forEach((el) => observer.observe(el));
